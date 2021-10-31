843551dd1cb9a52b0d5b95c17968a884d77bd87c
Gustavo-tech <gustavosh31@gmail.com>
2021-10-30 21:09:16 -0300

    Adding book author model

---
+++ LivrariaAmazonia/src/Database/BookAuthorsDao.java
@@ -0,0 +1,99 @@
+package Database;
+
+import Model.Author;
+import Model.BookAuthor;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookAuthorsDao {
+    private static final String URL = DatabaseConstants.URL;
+    private static final String USER = DatabaseConstants.USER;
+    private static final String PASS = DatabaseConstants.PASS;
+    
+    public static List<BookAuthor> getBookAuthors() {
+        List<BookAuthor> bookAuthors = new ArrayList<BookAuthor>();
+        try(Connection con = DriverManager.getConnection(URL, USER, PASS)) {
+            String query = "SELECT * FROM booksauthors";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            
+            while(rs.next()) {
+                String isbn = rs.getString("isbn");
+                Integer authorId = rs.getInt("author_id");
+                Integer seqNo = rs.getInt("seq_no");
+                
+                BookAuthor ba = new BookAuthor(isbn, authorId, seqNo);
+                bookAuthors.add(ba);
+            }
+            
+            con.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return bookAuthors;
+    }
+    
+    public static BookAuthor getBookAuthor(String isbn) {
+        try (Connection con = DriverManager.getConnection(URL, USER, PASS)) {
+            String query = "SELECT * FROM booksauthors WHERE isbn = ?";
+            PreparedStatement pstm = con.prepareStatement(query);
+            pstm.setString(1, isbn);
+            ResultSet rs = pstm.executeQuery();
+            
+            rs.next();
+            Integer authorId = rs.getInt("author_id");
+            Integer seqNo = rs.getInt("seq_no");
+            
+            return new BookAuthor(isbn, authorId, seqNo);
+        } catch(SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void addBookAuthor(BookAuthor bookAuthor) {        
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASS)) {
+          String query = "INSERT INTO booksauthors VALUES (?, ?, ?)";
+          PreparedStatement pstm = conn.prepareStatement(query);
+          pstm.setString(1, bookAuthor.getIsbn());
+          pstm.setInt(2, bookAuthor.getAuthorId());
+          pstm.setInt(3, bookAuthor.getSeqNo());
+          pstm.executeUpdate();
+          conn.close();
+        } catch(SQLException e) {
+          e.printStackTrace();
+        } 
+    }
+
+    public static void updateBookAuthor(BookAuthor bookAuthor) {
+        try(Connection conn = DriverManager.getConnection(URL, USER, PASS)) {
+          String query = "UPDATE booksauthors SET isbn = ?, author_id = ?, seq_no = ?";
+          PreparedStatement pstm = conn.prepareStatement(query);
+          pstm.setString(1, bookAuthor.getIsbn());
+          pstm.setInt(2, bookAuthor.getAuthorId());
+          pstm.setInt(3, bookAuthor.getSeqNo());
+          pstm.executeUpdate();
+          conn.close();
+        } catch(SQLException e) {
+          e.printStackTrace();
+        } 
+    }
+
+    public static void deleteBookAuthor(String isbn) {
+      try (Connection conn = DriverManager.getConnection(URL, USER, PASS)) {
+        String query = "DELETE FROM booksauthors WHERE isbn = ?";
+        PreparedStatement pstm = conn.prepareStatement(query);
+        pstm.setString(1, isbn);
+        pstm.executeUpdate();
+        conn.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+}
---
+++ LivrariaAmazonia/src/Model/BookAuthor.java
@@ -0,0 +1,39 @@
+package Model;
+
+public class BookAuthor {
+    private String isbn;
+    private Integer authorId;
+    private Integer seqNo;
+    
+    public BookAuthor(String isbn, Integer authorId, Integer seqNo) {
+        this.isbn = isbn;
+        this.authorId = authorId;
+        this.seqNo = seqNo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public Integer getSeqNo() {
+        return seqNo;
+    }
+
+    public void setSeqNo(Integer seqNo) {
+        this.seqNo = seqNo;
+    }
+    
+    
+}
